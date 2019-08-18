275ea18c7538c15cfd747c6d7c37d15d8dbc9039
remowote <cookedmilk@gmail.com>
2019-08-17 20:12:36 -0500

    made space for calendar

---
+++ calendar/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget{
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title:'Calendar',
+
+      home: new Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.pink[100],
+          title: Text('Stress Calendar')
+        ),
+        body: Center(
+          child: Text('Calendar Coming Soon'),
+        ),
+      ),
+    );
+  }
+}
